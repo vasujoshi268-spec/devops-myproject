7e43cd4340bb6aa2f3a26ada50b288816e8b4130
Vasudev Joshi <Vasudev.Joshi@corp.gsecl.co.in>
2026-04-01 14:35:06 +0530

    add secrets

--- app/app.py
+++ app/app.py
@@ -1,10 +1,30 @@
 from flask import Flask
+import psycopg2
 
 app = Flask(__name__)
 
+def get_db_connection():
+    conn = psycopg2.connect(
+        host="db",
+        database="mydb",
+        user="user",
+        password="password"
+    )
+    return conn
+
 @app.route('/')
 def home():
-    return "Hello vasu bhai 🚀"
+    return "Flask + PostgreSQL Connected 🚀"
+
+@app.route('/data')
+def data():
+    conn = get_db_connection()
+    cur = conn.cursor()
+    cur.execute("SELECT NOW();")
+    result = cur.fetchone()
+    cur.close()
+    conn.close()
+    return f"Database Time: {result}"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000, debug=True)
